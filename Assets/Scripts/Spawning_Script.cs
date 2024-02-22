d4ddc41a58839f56386f099df3f566d70ea63501
Kyler-Stolte <kstolt300@caledonian.ac.uk>
2024-02-22 15:30:56 +0000

    Enemies can now spawn randomly

--- Assets/Scripts/Spawning_Script.cs
+++ Assets/Scripts/Spawning_Script.cs
@@ -6,38 +6,35 @@ using UnityEngine;
 public class Spawning_Script : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] public GameObject Enemy1;
-    [SerializeField] public GameObject Enemy2;
-    [SerializeField] public GameObject Enemy3;
+    [SerializeField] public GameObject[] enemies;
 
+    public Transform spawnPoint;
 
-    public float SpawnTimer = 0f;
-    public float Delay = 0f;
-    public int NumSpawns = 0;
-    public int MaxSpawns;
+    private int Rand;
+    private int randPosition;
+
+    public float StartTimeBetweenSpawns;
+
+    private float TimeBetwSpawns;
     void Start()
     {
-        NumSpawns = MaxSpawns;
-        SpawnTimer = Time.time;
-        Random.Range(0, MaxSpawns);
+        TimeBetwSpawns = StartTimeBetweenSpawns;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (NumSpawns >1){
-            if (SpawnTimer <= 0)
-            {
-                Instantiate(Enemy1, transform.position, Quaternion.identity);
-            }
-            while (Delay > 0f)
-            {
-                    gameObject.SetActive(false);
-
-                    Instantiate(Enemy2, transform.position, Quaternion.identity);
-                    Instantiate(Enemy3, transform.position, Quaternion.identity);
-            }
-            }
+        if (TimeBetwSpawns <=0)
+        {
+            Rand = Random.Range(0, enemies.Length);
+            randPosition = Random.Range(0, randPosition);
+            Instantiate(enemies[Rand],transform.position, Quaternion.identity);
+            TimeBetwSpawns = StartTimeBetweenSpawns;
+        }
+        else
+        {
+            TimeBetwSpawns -= Time.deltaTime;
         }
     }
+}
 
