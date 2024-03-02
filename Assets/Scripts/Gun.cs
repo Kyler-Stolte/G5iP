93ec078c5faa2575c1f99a944a31fe9e9a40d0f4
Kyler-Stolte <kstolt300@caledonian.ac.uk>
2024-03-02 22:36:34 +0000

    animation started

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+
 public class Gun : MonoBehaviour
 {
+    public Animator animator;
     private void FixedUpdate()
     {
         Vector3 mousePosition = Input.mousePosition;
@@ -12,9 +14,18 @@ public class Gun : MonoBehaviour
         //gun tracks mouse
         Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
         transform.up = direction;
+
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            animator.SetBool("IsClicked", true);
+        }
+        else
+        {
+            animator.SetBool("IsNotClicked", true);
+        }
     }
-
-   
-
-
 }
+
+    
+
+       
