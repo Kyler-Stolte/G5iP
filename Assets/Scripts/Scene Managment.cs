bbb91a32da40034738c5efe791dcb5f0748c5761
Douglas Gammons <dgammo300@caledonian.ac.uk>
2024-03-05 17:33:16 +0000

    Quick scene management script for UI
    Scene management Script

---
+++ Assets/Scripts/Scene Managment.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagment : MonoBehaviour
+{
+   public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName); // Load's named scene
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitted");
+        Application.Quit(); // Closes the application
+    }
+
+}
