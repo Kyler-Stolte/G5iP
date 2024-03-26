151ea303c16f66bffd18360a33e83f5a7b3e6dd1
Stolte <KSTOLT300@caledonian.ac.uk>
2024-03-26 14:18:20 +0000

    Fixed animations ft steven

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,37 +1,31 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using Unity.VisualScripting;
+
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.UIElements;
+
 
 public class Gun : MonoBehaviour
 {
     public Animator animator;
 
     public Transform Tracker;
+    public float Angle;
     private void Start()
     {
         
         Tracker.position = transform.position;
     }
-    private void FixedUpdate()
-    {
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        //gun tracks mouse
-        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
-       // transform.up = direction;
-      
 
-    }
     private void Update()
     {
 
-       // Vector3 Angle = new Vector3(0, 0, transform.rotation.z);
-       // animator.SetFloat("Angle", transform.rotation.eulerAngles.z);
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        //gun tracks mouse
+        Vector2 direction = new Vector2(mousePosition.x - Tracker.transform.position.x, mousePosition.y - Tracker.transform.position.y);
+        Tracker.transform.up = direction;
+
+        Angle = Tracker.transform.rotation.z;
+        animator.SetFloat("Angle", Angle);
+
         if (Input.GetMouseButtonDown(0) == true)
         {
             animator.SetBool("IsClicked", true);
